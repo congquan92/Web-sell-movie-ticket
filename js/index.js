f9bec88f66cc2412ab257bdfa60d353839d47621
LeHuuHuy020105 <lhhuy.2005@gmail.com>
2024-11-04 13:10:30 +0700

    huy

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const logregBox = document.querySelector(".logreg-box");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+registerLink.addEventListener("click", () => {
+  logregBox.classList.add("active");
+});
+loginLink.addEventListener("click", () => {
+  logregBox.classList.remove("active");
+});
+const login = document.querySelector(".login-active");
+const formlogin = document.querySelector(".box-login");
+const midcontent = document.querySelector(".mid-content");
+login.addEventListener("click", () => {
+  midcontent.style.display = "none";
+  formlogin.classList.add("active");
+});
+// Lấy tất cả các phần tử có lớp .close-loginbox
+const closeloginbox = document.querySelectorAll(".close-loginbox");
+
+// Gắn sự kiện click cho từng phần tử trong danh sách
+closeloginbox.forEach((element) => {
+  element.addEventListener("click", () => {
+    // Giả sử formlogin và midcontent đã được định nghĩa trước đó
+    formlogin.style.display = "none";
+    midcontent.style.display = "block";
+  });
+});
