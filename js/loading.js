5750dcbcf94dd09a5c0cee6236c88f6ce9a30371
congquan92 <145172432+congquan92@users.noreply.github.com>
2024-11-08 21:30:53 +0700

    responsive shop 8/11 9:30pm

--- js/loading.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var loading = document.getElementById("loading");
-  window.addEventListener("load", function () {
-    loading.style.display = "none";
-  });
-});
-// an thanh navbar
