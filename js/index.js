6086507de301ef421faf960877ea33ac56f9dfb3
LeHuuHuy020105 <lhhuy.2005@gmail.com>
2024-11-08 13:21:38 +0700

    huy_1108

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ const logonav = document.querySelector(".logo");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 const home = document.querySelector("#home"); //nut Home tren nav
+const shop = document.querySelector("#shop");
+const address_btn = document.querySelector(".address-btn");
+const content_addressbtn = document.querySelector("#footer-paragraph");
+const social_btn = document.querySelector(".social-btn");
+const content_socialbtn = document.querySelector("#footer-social-ul");
+const policy_btn = document.querySelector(".policy-btn");
+const content_policybtn = document.querySelector("#footer-policy-ul");
 registerLink.addEventListener("click", () => {
   logregBox.classList.add("active");
 });
@@ -31,9 +38,49 @@ close_nav.addEventListener("click", () => {
 });
 
 home.addEventListener("click", () => {
-  location.reload();
+  window.location.href = "index.html";
 });
 
 logonav.addEventListener("click", () => {
-  location.reload();
+  window.location.href = "index.html";
+});
+shop.addEventListener("click", () => {
+  window.location.href = "shop.html";
+});
+//  footer
+let flag_address_btn = 0;
+address_btn.addEventListener("click", () => {
+  if (flag_address_btn == 0) {
+    address_btn.classList.add("active");
+    content_addressbtn.classList.add("active");
+    flag_address_btn = 1;
+  } else {
+    address_btn.classList.remove("active");
+    content_addressbtn.classList.remove("active");
+    flag_address_btn = 0;
+  }
+});
+let flag_social_btn = 0;
+social_btn.addEventListener("click", () => {
+  if (flag_social_btn == 0) {
+    social_btn.classList.add("active");
+    content_socialbtn.classList.add("active");
+    flag_social_btn = 1;
+  } else {
+    social_btn.classList.remove("active");
+    content_socialbtn.classList.remove("active");
+    flag_social_btn = 0;
+  }
+});
+let flag_policy_btn = 0;
+policy_btn.addEventListener("click", () => {
+  if (flag_policy_btn == 0) {
+    policy_btn.classList.add("active");
+    content_policybtn.classList.add("active");
+    flag_policy_btn = 1;
+  } else {
+    policy_btn.classList.remove("active");
+    content_policybtn.classList.remove("active");
+    flag_policy_btn = 0;
+  }
 });
