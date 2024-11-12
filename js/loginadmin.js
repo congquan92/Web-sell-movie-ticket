234a7d84b246cf7b798385155a36cbbcf7ce47db
congquan92 <145172432+congquan92@users.noreply.github.com>
2024-11-12 22:46:12 +0700

    add admin 12/11 10:46pm

--- js/loginadmin.js
+++ js/loginadmin.js
@@ -1,6 +1,11 @@
-const email = document.querySelector('.input-box input[type="email"]')
-const password = document.querySelector('.input-box input[type ="password"]')
+const emailLogin = document.querySelector('.input-box input[type="email"]')
+const passwordLogin = document.querySelector('.input-box input[type ="password"]')
+const submitLogin = document.querySelector('button[type=submit]')
+const x = document.querySelector('button[type=submit]')
 const submit = document.querySelector('button[type=submit]')
+// const submit = document.querySelector('button[type=submit]')
+
+
 
 submit.addEventListener('click',(e)=>{
     if(email.value==='admin' && password.value ==='123456'){
