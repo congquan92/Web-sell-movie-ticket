02a99c53802d9325a5612af590888c75447718f2
LeHuuHuy020105 <lhhuy.2005@gmail.com>
2024-11-14 17:27:18 +0700

    huy1114_5h27

--- js/admin.js
+++ js/admin.js
@@ -1,33 +1,52 @@
-const QLTK = document.querySelector('.b1')
-const QLDH = document.querySelector('.b2')
-const QLSP = document.querySelector('.b3')
-const QLND = document.querySelector('.b4')
-// console.log(QLTK, QLDH, QLSP, QLND); 
+const QLTK = document.querySelector(".b1");
+const QLDH = document.querySelector(".b2");
+const QLSP = document.querySelector(".b3");
+const QLND = document.querySelector(".b4");
+// console.log(QLTK, QLDH, QLSP, QLND);
 
-QLTK.addEventListener('click',()=>{
-    QLTK.classList.add('act');
-    QLDH.classList.remove('act');
-    QLSP.classList.remove('act');
-    QLND.classList.remove('act');  
-})
-QLDH.addEventListener('click',()=>{
-    QLTK.classList.remove('act');
-    QLDH.classList.add('act');
-    QLSP.classList.remove('act');
-    QLND.classList.remove('act');
-})
+QLTK.addEventListener("click", () => {
+  QLTK.classList.add("act");
+  QLDH.classList.remove("act");
+  QLSP.classList.remove("act");
+  QLND.classList.remove("act");
+  leftpage.classList.remove("active");
+  backgroudpage.style.display = "none";
+});
+QLDH.addEventListener("click", () => {
+  QLTK.classList.remove("act");
+  QLDH.classList.add("act");
+  QLSP.classList.remove("act");
+  QLND.classList.remove("act");
+  leftpage.classList.remove("active");
+  backgroudpage.style.display = "none";
+});
 
 //sp
-QLSP.addEventListener('click',()=>{
-    QLTK.classList.remove('act');
-    QLDH.classList.remove('act');
-    QLSP.classList.add('act');
-    QLND.classList.remove('act');
-
-})
-QLND.addEventListener('click',()=>{
-    QLTK.classList.remove('act');
-    QLDH.classList.remove('act');
-    QLSP.classList.remove('act');
-    QLND.classList.add('act');
-})
+QLSP.addEventListener("click", () => {
+  QLTK.classList.remove("act");
+  QLDH.classList.remove("act");
+  QLSP.classList.add("act");
+  QLND.classList.remove("act");
+  leftpage.classList.remove("active");
+  backgroudpage.style.display = "none";
+});
+QLND.addEventListener("click", () => {
+  QLTK.classList.remove("act");
+  QLDH.classList.remove("act");
+  QLSP.classList.remove("act");
+  QLND.classList.add("act");
+  leftpage.classList.remove("active");
+  backgroudpage.style.display = "none";
+});
+//huy
+const leftpage = document.querySelector(".page-left");
+const backgroudpage = document.querySelector(".backgroud-menu-respon");
+const menu = document.querySelector(".menu1");
+menu.addEventListener("click", () => {
+  leftpage.classList.add("active");
+  backgroudpage.style.display = "block";
+});
+backgroudpage.addEventListener("click", () => {
+  leftpage.classList.remove("active");
+  backgroudpage.style.display = "none";
+});
