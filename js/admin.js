8f3cf0b8ecae0c8d997dd5ca5d98906cc2cc8040
congquan92 <145172432+congquan92@users.noreply.github.com>
2024-11-16 06:49:02 +0700

    update 16/11 6:48am

--- js/admin.js
+++ js/admin.js
@@ -14,6 +14,7 @@ let ArrProduct = [
       img1: "./img/products/p1-1.png",
       img2: "./img/products/p1-2.jpg",
       img3: "./img/products/p1-2.jpg",
+      sell:2
     },
     {
       nameSP: "LEVENTS® | DORAEMON COLLAB TEE",
@@ -30,6 +31,7 @@ let ArrProduct = [
       img1: "./img/products/p2-1.png",
       img2: "./img/products/p2-2.png",
       img3: "./img/products/p2-2.png",
+      sell :2
     },
     {
       nameSP: "LEVENTS® DINOSAUR TEE",
@@ -46,6 +48,7 @@ let ArrProduct = [
       img1: "./img/products/p3-1.jpg",
       img2: "./img/products/p3-2.jpg",
       img3: "./img/products/p3-3.jpg",
+      sell :7
     },
     {
       nameSP: "LEVENTS® INSIDE OUT TEE",
@@ -62,6 +65,7 @@ let ArrProduct = [
       img1: "./img/products/p4-1.jpg",
       img2: "./img/products/p4-2.jpg",
       img3: "./img/products/p4-2.jpg",
+      sell :0
     },
     {
       nameSP: "LEVENTS® LOVEYOU300K SPECIAL TEE",
@@ -78,6 +82,7 @@ let ArrProduct = [
       img1: "./img/products/p5-1.jpg",
       img2: "./img/products/p5-1.jpg",
       img3: "./img/products/p5-1.jpg",
+      sell : 0
     },
     {
       nameSP: "LEVENTS® FUNNY CROCODILE TEE",
@@ -94,6 +99,7 @@ let ArrProduct = [
       img1: "./img/products/p6-1.jpg",
       img2: "./img/products/p6.jpg",
       img3: "./img/products/p6-2.jpg",
+      sell : 0
     },
     {
       nameSP: "LEVENTS® PUNCH VARSITY",
@@ -110,6 +116,7 @@ let ArrProduct = [
       img1: "./img/products/p7-1.jpg",
       img2: "./img/products/p7-2.jpg",
       img3: "./img/products/p7-2.jpg",
+      sell : 9
     },
     {
       nameSP: "LEVENTS® SELFLOVE BOXY TEE",
@@ -126,6 +133,7 @@ let ArrProduct = [
       img1: "./img/products/p8-1.jpg",
       img2: "./img/products/p8-2.jpg",
       img3: "./img/products/p8-1.jpg",
+      sell :0
     },
     {
       nameSP: "LEVENTS® | DORAEMON MINI CAT Polo",
@@ -142,6 +150,7 @@ let ArrProduct = [
       img1: "./img/products/p9-1.jpg",
       img2: "./img/products/p9-2.jpg",
       img3: "./img/products/p9-2.jpg",
+      sell : 2
     },
     {
       nameSP: "LEVENTS® MINI POPULAR Polo",
@@ -158,6 +167,7 @@ let ArrProduct = [
       img1: "./img/products/p10-1.jpg",
       img2: "./img/products/p10-2.jpg",
       img3: "./img/products/p10-2.jpg",
+      sell : 8
     },
     {
       nameSP: "LEVENTS® STRIPE Polo",
@@ -174,6 +184,7 @@ let ArrProduct = [
       img1: "./img/products/p11-1.jpg",
       img2: "./img/products/p11-2.jpg",
       img3: "./img/products/p11-2.jpg",
+      sell : 2
     },
     {
       nameSP: "LEVENTS® CINEMA SHIRT",
@@ -190,6 +201,7 @@ let ArrProduct = [
       img1: "./img/products/p12-1.jpg",
       img2: "./img/products/p12-2.jpg",
       img3: "./img/products/p12-2.jpg",
+      sell : 5
     },
     {
       nameSP: "LEVENTS® CITIES SHIRT",
@@ -206,6 +218,7 @@ let ArrProduct = [
       img1: "./img/products/p13-1.jpg",
       img2: "./img/products/p13-2.jpg",
       img3: "./img/products/p13-2.jpg",
+      sell : 6
     },
     {
       nameSP: "LEVENTS® | DORAEMON COLLAB ZIPPER Hoodie",
@@ -222,6 +235,7 @@ let ArrProduct = [
       img1: "./img/products/p14-1.jpg",
       img2: "./img/products/p14-2.jpg",
       img3: "./img/products/p14-2.jpg",
+      sell : 2
     },
     {
       nameSP: "LEVENTS® POPULAR LOGO 2.0 Hoodie",
@@ -238,6 +252,7 @@ let ArrProduct = [
       img1: "./img/products/p15-1.jpg",
       img2: "./img/products/p15-2.jpg",
       img3: "./img/products/p15-3.jpg",
+      sell : 7
     },
     {
       nameSP: "LEVENTS® MINI LOGO ZIPPER Hoodie",
@@ -254,6 +269,7 @@ let ArrProduct = [
       img1: "./img/products/p16-1.jpg",
       img2: "./img/products/p16-2.jpg",
       img3: "./img/products/p16-2.jpg",
+      sell : 2
     },
     {
       nameSP: "LEVENTS® | DORAEMON COLLAB Hoodie",
@@ -270,6 +286,7 @@ let ArrProduct = [
       img1: "./img/products/p17-1.jpg",
       img2: "./img/products/p17-2.jpg",
       img3: "./img/products/p17-2.jpg",
+      sell : 1
     },
     {
       nameSP: "LEVENTS® BASIC Sweater",
@@ -286,6 +303,7 @@ let ArrProduct = [
       img1: "./img/products/p18-1.jpg",
       img2: "./img/products/p18-2.jpg",
       img3: "./img/products/p18-2.jpg",
+      sell : 9
     },
     {
       nameSP: "LEVENTS® FUNNY CROCODILE Sweater",
@@ -302,9 +320,10 @@ let ArrProduct = [
       img1: "./img/products/p19-1.jpg",
       img2: "./img/products/p19-2.jpg",
       img3: "./img/products/p19-2.jpg",
+      sell : 3
     },
   ];
-  let typeproducts = [
+let typeproducts = [
     { typeid: "aothun#", typename: "Áo thun" },
     { typeid: "polo#", typename: "Polo" },
     { typeid: "somi#", typename: "Sơ mi" },
@@ -313,14 +332,12 @@ let ArrProduct = [
     { typeid: "aokhoac#", typename: "Áo khoác" },
   ];
 
-
 // Hàm tạo id SP
 function makeIDproduct() {
   for (let i = 0; i < ArrProduct.length; i++) {
     ArrProduct[i].idproduct = ArrProduct[i].nametag + (i + 1);
   }
 }
-
 //----------------sp-------------------------
 // Hàm xác định trạng thái sản phẩm
 function productSatus() {
@@ -385,18 +402,92 @@ function searchSP(){
   document.querySelector('#storage-body').innerHTML= listSP(filteredProducts);
   document.querySelector('#amountOfProduct').innerText= countProduct(filteredProducts);
 }
+// Hàm tạo biểu đồ
+const listSweater = ArrProduct.filter(i => i.nametag === 'sweater#');
+const listSomi = ArrProduct.filter(i => i.nametag === 'somi#');
+const listHoodie = ArrProduct.filter(i => i.nametag === 'hoodie#');
+const listAokhoac = ArrProduct.filter(i => i.nametag === 'aokhoac#');
+const listAothun = ArrProduct.filter(i => i.nametag === 'aothun#');
+
+const namee = ['Áo khoác', 'Sweater', 'Hoodie', 'Áo thun', 'Sơ mi'];
+const sumList = [
+    listAokhoac.reduce((i, x) => i + x.sell, 0),
+    listSweater.reduce((i, x) => i + x.sell, 0),
+    listHoodie.reduce((i, x) => i + x.sell, 0),
+    listAothun.reduce((i, x) => i + x.sell, 0),
+    listSomi.reduce((i, x) => i + x.sell, 0),
+];
+const Moneylist =[
+  listAokhoac.reduce((i, x) => i + (x.sell) *890000, 0),
+  listSweater.reduce((i, x) => i + (x.sell) *490000, 0),
+  listHoodie.reduce((i, x) => i + (x.sell) *690000, 0),
+  listAothun.reduce((i, x) => i + (x.sell) *380000, 0),
+  listSomi.reduce((i, x) => i + (x.sell) *420000, 0),
+];
+
+function createChart() {
+  const ctx = document.getElementById('grapbox').getContext('2d');
+  const chartData = {
+    labels: namee ,
+    datasets: [{
+      label: 'Sales Data',
+      data: sumList ,
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  };
+  new Chart(ctx, {
+    type: 'polarArea',
+    data: chartData,
+    options: {}
+  });
+}
+function renderqltk(){
+  document.querySelector('.page-right').innerHTML =`<div class = qltk>
+  <h1>SỐ LIỆU THỐNG KÊ</h1>
+  <div class="overview">
+      <div class="chart ff"><canvas style="display: block; box-sizing: border-box;" id="grapbox" width="541px" height="541px" ></canvas></div>
+      <div class="ll">
+         <div class="boder first">
+          <h1>Thống kê</h1>    
+         </div> 
+         <div class="boder">
+              <div class="left-boder">
+                  <h2 style="color: blue;">${sumList.reduce((i,n) => i+n,0)}</h2>
+                  <h2 style="font-weight: 200;">ĐÃ BÁN</h2> 
+              </div>
+              <div class="right-boder"><i style="font-size: 40px;font-weight: 200;" class='bx bx-cart-alt'></i></div>   
+         </div> 
+         <div class="boder">
+              <div class="left-boder">
+                  <h2 style="color: blue;">${(Moneylist.reduce((i,n) => i+n,0)).toLocaleString("vi-VN", {style: "currency",currency: "VND",})}</h2>
+                  <h2 style="font-weight: 200;">DOANH THU</h2>
+              </div>
+              <div class="right-boder"><i style="font-size: 40px;font-weight: 200;" class='bx bx-money-withdraw'></i></div>   
+         </div> 
+      </div>
+   </div>
+</div>`
+createChart(); // Tạo biểu đồ khi nhấp vào "QUẢN LÝ THỐNG KÊ"
+}
 // -------------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", () => {
     const QLTK = document.querySelector('.b1');
     const QLDH = document.querySelector('.b2');
     const QLSP = document.querySelector('.b3');
     const QLND = document.querySelector('.b4');
-  
+    
     QLTK.addEventListener('click', () => {
       QLTK.classList.add('act');
       QLDH.classList.remove('act');
       QLSP.classList.remove('act');
       QLND.classList.remove('act');
+      renderqltk();
     });
   
     QLDH.addEventListener('click', () => {
