2249c94f92fb68f759a53d2df0c36d9c82312769
thanh thuy <thanhthuyy1010@gmail.com>
2024-11-19 15:43:15 +0700

    19/11/2024 3:42pm

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const newclothes = document.querySelector('.newclothes');
     const addresses = document.querySelectorAll('.addr');
-
+    const cities = document.querySelector('.city');
     function fadeDown() {
-        newclothes.classList.add('visible')
+        newclothes.classList.add('fadedown');
     }
     function fadeRight() {
         addresses.forEach(address => {
-            const rect = address.getBoundingClientRect();
-            if(rect.top < window.innerHeight && rect.bottom > 0) {
-                address.classList.add('open');
+            const rect = address.getBoundingClientRect(); // lay thong tin vi tri va kich thuoc cua phan tu
+            if(rect.top < window.innerHeight) {
+                address.classList.add('faderight');
+            }
+        })
+    }
+    function flipLeft() {
+        cities.forEach(city => {
+            const rect = city.getBoundingClientRect();
+            if(rect.top < window.innerHeight) {
+                city.classList.add('flipleft');
             }
         })
     }
